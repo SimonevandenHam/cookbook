ac7d3b5caba72641fce83c07c551cbdb9ac84f80
SimonevandenHam <sae.vandenham@gmail.com>
2020-05-27 14:51:40 +0200

    created recipe model and relations

---
+++ recipe/model.js
@@ -0,0 +1,49 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const User = require("../userCreate/model");
+const Category = require("../categories/model");
+
+const Recipe = db.define(
+  "recipe",
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    cookingTime: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    recipeDiscription: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    ingredients: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    recipeNotes: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    isFavorite: {
+      type: Sequelize.SMALLINT,
+    },
+    numberOfServings: {
+      type: Sequelize.INTEGER,
+    },
+  },
+  {
+    tableName: "Recipies",
+  }
+);
+
+//RELATION
+Recipe.belongsTo(User);
+User.hasMany(Recipe);
+
+Recipe.belongsTo(Category);
+Category.hasMany(Recipe);
+
+module.exports = Recipe;
